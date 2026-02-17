065ebd32bf9b61460689a9a62eb5017d5672c0e0
Javier Rebull <168046724+jrebull@users.noreply.github.com>
2026-02-17 14:28:53 -0500

    HHO Newone 2

--- SmartOpt/HHO_Phase01/presentation-redesign/next.config.mjs
+++ SmartOpt/HHO_Phase01/presentation-redesign/next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/smartopt/hho_phase01/presentation-redesign/out',
+  assetPrefix: '/smartopt/hho_phase01/presentation-redesign/out',
   typescript: {
     ignoreBuildErrors: true,
   },
